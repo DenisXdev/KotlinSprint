3589634e9e86c580e8c0f1843bee25cd12030b0c
Berdiev <k999773k@gmail.com>
2024-11-12 23:12:08 +0400

    KS-1-2 create smth

---
+++ app/src/main/java/lesson_1/lesson1_task2.kt
@@ -0,0 +1,17 @@
+package lesson_1
+
+fun main(){
+
+    val numOrders = 75
+    val orderThanks = "Благодарим за покупку! Ваша поддержка помогает нам расти и становиться лучше. Мы уже обрабатываем ваш заказ и вскоре он будет на пути к вам. Приятных покупок!"
+
+    println(numOrders)
+    println(orderThanks)
+
+//    var numWorkers: Int = 2000
+//    println(numWorkers)
+
+    var numWorkers = 1999
+
+    println(numWorkers)
+}
